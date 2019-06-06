d4e9ad0d69cf5784c8512ebee2927ffb2a77c4f6
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-07 01:35:04 +0530

    third commit

--- index.php
+++ index.php
@@ -20,7 +20,31 @@ $connect = mysqli_connect("localhost", "root", "", "shopping");
     <br />
     <div class="container" style="width: 700px;">
         <h3 align="center">Shopping Cart</h3><br />
-
+        <?php
+            $query = "SELECT * FROM products ORDER BY id ASC";
+            $result = mysqli_query($connect, $query);
+            if(mysqli_num_rows($result)>0)
+            {
+                while(mysqli_fetch_array($result))
+                {
+            ?>
+            <div class="col-md-4">
+                <form method="post" action = "index.php?action=add&id=<?php echo $row["id"]; ?>">
+                    <div style = "border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align = "center">
+                        <img src = "images/<?php echo $row["image"]; ?>" class = "img-responsive" /><br />
+                        <h4 class = "text-info"><?php echo $row["name"]; ?></h4>
+                        <h4 class = "text-danger">$ <?php echo $row["price"]; ?></h4>
+                        <input type = "text" name = "quantity" value="1" class = "form-control" />
+                        <input type = "hidden" name = "hidden_name" value = "<?php echo $row["name"]; ?>" />
+                        <input type = "hidden" name = "hidden_price" value = "<?php echo $row["price"]; ?>" />
+                        <input type = "submit" name = "add_to_cart" style = "margin-top:5px;" class = "btn btn-success" value = "Add to Cart" />
+                    </div>
+                </form>
+            </div>
+        <?php
+                }
+            }
+        ?>
 
     </div>
     <br />
