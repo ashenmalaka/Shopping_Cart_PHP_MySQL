611d951aa51911f25144b90e674b52fdb0cf139b
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-07 00:54:34 +0530

    second commit

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <?php
+session_start();
+$connect = mysqli_connect("localhost", "root", "", "shopping");
 
 
 
 ?>
 
-<html>
-<head>
 
-</head>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Shopping Cart | PHP & MySQL</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
 <body>
+    <br />
+    <div class="container" style="width: 700px;">
+        <h3 align="center">Shopping Cart</h3><br />
+
+
+    </div>
+    <br />
 
 </body>
+
+
 
 </html>
