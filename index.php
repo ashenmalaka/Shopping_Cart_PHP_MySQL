4dd2e7d6553421306e239735c9522b9f850074a6
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-06-07 22:17:58 +0530

    sixth commit

--- index.php
+++ index.php
@@ -17,13 +17,11 @@ if (isset($_POST["add_to_cart"]))
                 'item_quantity'		=>	$_POST["quantity"]
             );
             $_SESSION["shopping_cart"][$count] = $item_array;
-
         }else
             {
                 echo '<script>alert("Item is already added")</script>';
                 echo '<script>window.location = "index.php"</script>';
             }
-
     }else
         {
             $item_array = array(
@@ -33,25 +31,20 @@ if (isset($_POST["add_to_cart"]))
                 'item_quantity'		=>	$_POST["quantity"]
             );
             $_SESSION["shopping_cart"][0] = $item_array;
-
         }
-
 }
-
-
-
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
+
     <head>
         <title>My Shopping Cart | PHP & MySQL</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </head>
+
 <body>
     <br />
     <div class="container" style="width: 700px;">
@@ -83,8 +76,47 @@ if (isset($_POST["add_to_cart"]))
             }
         ?>
 
-    </div>
-    <br />
+    <div class="clear:both"></div>
+        <br />
+        <h3>Order Details</h3>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <tr>
+                    <th width="40%">Item Name</th>
+                    <th width="10%">Quantity</th>
+                    <th width="20%">Price</th>
+                    <th width="15%">Total</th>
+                    <th width="5%">Action</th>
+                </tr>
 
+                <?php
+                if(!empty($_SESSION["shopping_cart"]))
+                {
+                    $total = 0;
+                    foreach ($_SESSION["shopping_cart"] as $keys => $values)
+                    {
+                ?>
+                <tr>
+                    <td><?php echo $values["item_name"]; ?></td>
+                    <td><?php echo $values["item_quantity"]; ?></td>
+                    <td>$ <?php echo $values["item_price"]; ?></td>
+                    <td>$ <?php echo number_format($values["item_quantity"]*$values["item_price"], 2);?></td>
+                    <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+                </tr>
+                <?php
+                    $total = $total + ($values["item_quantity"]*$$values["item_price"]);
+                    }
+                ?>
+
+
+                <?php
+                }
+                ?>
+
+            </table>
+        </div>
+    </div>
+
+    <br />
 </body>
 </html>
